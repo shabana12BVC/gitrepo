c63edf1d47c5fc3f5e3388ef1aebd7c95135e9e0
shabana <shabanafsh@gmail.com>
2021-10-28 14:10:29 +0530

    this is first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,render_template
+app=Flask(__name__)
+
+@app.route("/")
+def hello_flask():
+    return "hello Flask!!"
+@app.route("/index")
+def index():
+    pass
+    
+
+if __name__=='__main__':
+    
+    app.run (debug = True)
+
+    
+    
+    
+    
+    
